586dc8ff6dff9c9a9b75fee1952729f9f315d7fc
Mateus Souza da Silva <mateus.ssilva2002@gmail.com>
2023-08-09 21:10:35 -0300

    Add files via upload

---
+++ aula09-08-2023/fatec/backend/calculadora_simples.php
@@ -0,0 +1,9 @@
+<?php
+    $valor1 = $_GET["valorA"];
+    $valor2 = $_GET["valorB"];
+
+    $soma = $valor1 + $valor2;
+
+    echo "<center><h1>$soma</center>";
+
+?>
---
+++ aula09-08-2023/fatec/backend/calculadora_soma.php
@@ -0,0 +1,10 @@
+<?php
+    $valorA = 5;
+    $valorB = 10;
+
+    $soma = $valorA + $valorB;
+
+    echo "$soma";
+    echo "<br>Com concatenação: A soma de ".$valorA." + ".$valorB." é de: ".$soma; //concatenaçao
+    echo "<br>Com interpolação: A soma de $valorA + $valorB é de: $soma"; //interpolação
+?>
